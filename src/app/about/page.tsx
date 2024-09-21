b61ee71425633114d3d53c4489fe42b158af4d32
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-09-21 15:39:02 +0900

    add:static-rendering

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,8 @@
-// npx create-next-app@latest アプリ名
-// import { useState } from 'react';
-
+// 静的レンダリングは、アプリケーションのビルド時にHTMLページを生成する方法
+// ウェブページに必要なすべてのデータが事前に準備されるためパフォーマンスが向上する
+// .nextフォルダを削除しておく
+// npm run buildを実行
+// npm run startを実行（デフォルトでキャッシュが有効化されている）
 export default function AboutPage() {
-  // サーバーコンポーネントでこれはできない
-  // const [name, setName] = useState('');
-  // コンソールはターミナルに表示される（デフォルトでサーバーコンポーネントになっている）
-  console.log('サーバーコンポーネント');
-
-  return <h1>About Page</h1>;
+  return <h1>About Page {new Date().toLocaleTimeString()}</h1>;
 }
