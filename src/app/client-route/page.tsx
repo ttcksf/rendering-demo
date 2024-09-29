e24e9d075bfdf602474a64d32da8e17c1b47ea24
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-09-29 17:42:05 +0900

    third-party

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -1,9 +1,35 @@
 'use client';
+// npm i react-slick slick-carousel @types/react-slick
+// playgroundからコピペ
 
-// import { serverSideFunction } from '@/utils/server-utils';
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+// gloabal.cssに記載するため
+// import './ImageSlider.css';
 
 export default function Page() {
-  // const result = serverSideFunction();
-  // return <h1>ClientPage:{result}</h1>;
-  return <h1>ClientPage</h1>;
+  const settings = {
+    dots: true,
+  };
+  return (
+    <div className="image-slider-container">
+      <Slider {...settings}>
+        <div>
+          {/* picsum.photosに変更 */}
+          <img src="http://picsum.photos/400/200" />
+        </div>
+        <div>
+          <img src="http://picsum.photos/400/200" />
+        </div>
+        <div>
+          <img src="http://picsum.photos/400/200" />
+        </div>
+        <div>
+          <img src="http://picsum.photos/400/200" />
+        </div>
+      </Slider>
+    </div>
+  );
 }
